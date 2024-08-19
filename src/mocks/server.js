08f7130f75ca2ab0112a4d97c841d38a36a7901d
Rushion <rushionchegge@gmail.com>
2024-08-20 01:40:47 +0300

    tests passing

--- src/mocks/server.js
+++ src/mocks/server.js
@@ -1,4 +1,14 @@
-import { setupServer } from "msw/node";
-import { handlers } from "./handlers";
+import { rest } from "msw";
 
+export const handlers = [
+  rest.get("https://dog.ceo/api/breeds/image/random", (req, res, ctx) => {
+    return res(
+      ctx.json({
+        message: "https://images.dog.ceo/breeds/bulldog-english/mami.jpg",
+      })
+    );
+  }),
+];
+
+import { setupServer } from "msw/node";
 export const server = setupServer(...handlers);
